bcc1f8f1ef139071d16d21e003f1c8464ed88c2a
LiFei <18911912840@163.com>
2018-12-18 17:24:20 +0800

    问题: 推送相关 原因: 解决方案: Reviewer: 工单号: X Log: X

--- MiWei/AppDelegate.m
+++ MiWei/AppDelegate.m
@@ -13,12 +13,13 @@
 #import "WMUIUtility.h"
 #import <WXApi.h>
 #import <Bugly/Bugly.h>
+#import <UMCommon/UMCommon.h>
 #import <UMPush/UMessage.h>
 
 NSString *const WMWechatAuthNotification = @"WMWechatAuthNotification";
 NSString *const WMWechatPayNotification = @"WMWechatPayNotification";
 
-@interface AppDelegate () <WXApiDelegate>
+@interface AppDelegate () <WXApiDelegate, UNUserNotificationCenterDelegate>
 @end
 
 @implementation AppDelegate
@@ -37,7 +38,17 @@ NSString *const WMWechatPayNotification = @"WMWechatPayNotification";
         WMNavigationViewController *nav = [[WMNavigationViewController alloc] initWithRootViewController:loginVC];
         self.window.rootViewController = nav;
     }
+    // 注册通知
+    if (@available(iOS 10.0, *)) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            
+        }];
+    } else {
+        // Fallback on earlier versions
+    }
     
+    [UMConfigure initWithAppkey:@"5b508417a40fa3453d00012a" channel:nil];
     // Push组件基本功能配置
     UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
     //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标
@@ -47,7 +58,49 @@ NSString *const WMWechatPayNotification = @"WMWechatPayNotification";
         }else{
         }
     }];
+//    [UMessage setAutoAlert:YES];
     return YES;
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+//    if (@available(iOS 10.0, *)) {
+//        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+//        center.delegate = self;
+//        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+//        content.title = [NSString localizedUserNotificationStringForKey:@"Hello" arguments:nil];
+//        content.body = [NSString localizedUserNotificationStringForKey:[NSString stringWithFormat:@"Agent-%d",arc4random()%100] arguments:nil];
+//        content.sound = [UNNotificationSound defaultSound];
+//        
+//        //    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:alertTime repeats:NO];
+//        UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:@"OXNotification" content:content trigger:nil];
+//        
+//        [center addNotificationRequest:request withCompletionHandler:^(NSError *_Nullable error) {
+//            NSLog(@"成功添加推送");
+//        }];
+//    }
+    
+    
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"cnm" message:@"ndy" delegate:nil cancelButtonTitle:@"关闭" otherButtonTitles:nil, nil];
+//    [alert show];
+    
+//    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+//
+//    localNotification.alertAction = @"cnm";
+//    localNotification.alertBody = @"ndy";
+//    [localNotification setSoundName:UILocalNotificationDefaultSoundName];
+//
+//
+//    // NSDictionary *dict = @{@"key1": [NSString stringWithFormat:@"%d",RC_KIT_LOCAL_NOTIFICATION_TAG]};
+//    //[localNotify setUserInfo:dict];
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
+//    });
+}
+
+#pragma mark - UNUserNotificationCenterDelegate
+// iOS 10收到通知
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler  API_AVAILABLE(ios(10.0)){
+    completionHandler(UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionSound|UNNotificationPresentationOptionAlert);
 }
 
 - (void)startRecordWithAppId:(NSString *__nullable)appId {
